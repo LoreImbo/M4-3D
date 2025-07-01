cb54d189e1172115ca24caf63c69fd475b05aeb5
Lorenzo Imbonati <96497366+LoreImbo@users.noreply.github.com>
2025-07-01 23:26:23 +0200

    lab3

--- Assets/ESERCIZI_LAB/Scripts/Lab3/PlayerController.cs
+++ Assets/ESERCIZI_LAB/Scripts/Lab3/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rb;
     [SerializeField] private float _speed = 3;
+    [SerializeField] private GroundCheck _groundCheck;
+    [SerializeField] private float _jumpForce = 5f;
+    private bool _canDoubleJump = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +30,27 @@ public class PlayerController : MonoBehaviour
             // Smoothly rotate towards the movement direction
             Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
+        }
+        
+        if (_groundCheck.IsGrounded)
+        {
+            _canDoubleJump = true; // Reset double jump when grounded
+        }
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (_groundCheck.IsGrounded)
+            {
+                // Primo salto
+                _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            }
+            else if (_canDoubleJump)
+            {
+                // Doppio salto
+                //_rb.velocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
+                _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+                _canDoubleJump = false;
+            }
         }
 
         _rb.MovePosition(_rb.position + direction * (_speed * Time.deltaTime));
